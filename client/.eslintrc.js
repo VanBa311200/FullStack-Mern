c5df234e1a29b59e36ae9ca0f0d1fbca28b07044
VanBa311200 <vanba311200@gmail.com>
2021-08-27 00:01:18 +0700

    Second commit

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['prettier', 'react-app'],
+  extends: ['prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,4 +1,7 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'something';
+export const apiUrl = process.env.NODE_ENV !== 'production' ?
+  'http://localhost:5000/'
+  :
+  'https://shrouded-everglades-40590.herokuapp.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern';
 
